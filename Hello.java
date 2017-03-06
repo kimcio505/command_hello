8e8af86f6a328b9b970e14863388a994910f1972
kimcio505 <kimcio505@naver.com>
2017-03-06 12:15:03 +0900

    Update Hello.java
    added gitHub "Tell gitHub World"

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello{
 		System.out.println("Tell Your World");
 		System.out.println("Tell his World");
 		System.out.println("Tell her World");
+		System.out.println("Tell gitHub World");
 	}
 }
 
